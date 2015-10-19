a91ac3a46cc5a9877d5f9bf55988e29425aa2d1a
Martin Hadley <martinjohnhadley@gmail.com>
2015-10-19 21:45:17 +0100

    Re-write of about page, in preparation for redesign of app

---
+++ external/aboutThisTool.R
@@ -0,0 +1,6 @@
+"We are using google spreadsheets and forms to make it easy for everyone in the delivery group to keep the information that underpins this site
+                        accurate and up to date. We are using a visualization service (",
+"<a href='http://www.shinyapps.io'>shinyapps.io</a>",
+") to read the data i.e. using Gantt chart views of projects, heat maps of teaching activity, and tree maps to show where our effort is focused.</p>",
+"For more information about RDM at Oxford please see the research data website: ",
+"<a href=http://researchdata.ox.ac.uk/home/introduction-to-rdm/>researchdata.ox.ac.uk/home/introduction-to-rdm/</a></p>",
